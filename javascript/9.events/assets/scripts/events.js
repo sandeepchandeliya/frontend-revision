2e4243187ecd18e871256da959363a18d47f4a98
sandeep chandeliya <sandeepchandeliya92@gmail.com>
2026-07-06 22:32:36 +0530

    more on events

--- javascript/9.events/assets/scripts/events.js
+++ javascript/9.events/assets/scripts/events.js
@@ -1,12 +1,30 @@
 const btn = document.querySelector('button');
+const form = document.querySelector('form');
+// const btnClickHandler = () => {
+//   console.log('button is clicked!!');
+// };
 
-const btnClickHandler = () => {
-  console.log('button is clicked!!');
-};
+// btn.addEventListener('click', btnClickHandler);
 
-btn.addEventListener('click', btnClickHandler);
+// setTimeout(() => {
+//   btn.removeEventListener('click', btnClickHandler);
+//   console.log('event lisener removed!!');
+// }, 2000);
 
-setTimeout(() => {
-  btn.removeEventListener('click', btnClickHandler);
-  console.log('event lisener removed!!');
-}, 2000);
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(event);
+});
+
+btn.addEventListener('click', (event) => {
+  event.stopPropagation();
+  console.log('button clicked');
+  console.log(event);
+});
+
+const div = document.querySelector('div');
+
+div.addEventListener('click', (event) => {
+  console.log('div clicked');
+  console.log(event);
+});
