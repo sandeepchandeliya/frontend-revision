0714a58b4c9e10f6af94d776dee96f239d8e6415
sandeep chandeliya <sandeepchandeliya92@gmail.com>
2026-07-13 23:39:43 +0530

    complete generics

--- typescript/5.generics/1.generics.ts
+++ typescript/5.generics/1.generics.ts
@@ -7,3 +7,24 @@ type DataStore<T> = {
 let store: DataStore<string | boolean> = {};
 store.name = 'max';
 store.isInstructor = true;
+
+function merge<T, U>(a: T, b: U) {
+  return [a, b];
+}
+
+let ids = merge(1, 'max');
+
+function mergeObj<T extends object, U extends object>(a: T, b: U) {
+  return { ...a, ...b };
+}
+
+const merged = mergeObj({ userName: 'Sandeep' }, { age: 31 });
+console.log(merged);
+
+class User<T> {
+  constructor(public id: T) {}
+}
+
+const newUser = new User('i1');
+
+newUser.id 
